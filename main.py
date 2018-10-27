0baf167584d2b7c41ec458d0bfee20035a907caf
door-bell <socaltdb@gmail.com>
2018-10-26 22:14:05 -0700

    create folders, once per student

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 
 import os
 
+# Set of student names:
+students = set()
+
+# Add students to set and make folders for each of them.
+for file in os.listdir("./"):
+    student = file.split("_")[0]
+    if (file.endswith('.java')):
+        students.add(student)
+
+
+    studentdir = "./" + student
+    if not os.path.isdir(studentdir):
+        os.mkdir(studentdir)
+        print("Folder created for: " + student)
